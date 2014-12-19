c57707c8fdd7988c96b722733bea17ca1638c623
Dolapo Toki <dolapotoki@gmail.com>
2014-12-19 11:25:28 -0800

    linked up the mainMenu Cards to their individual cards also added transition animation

---
+++ js/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    //This checkes what card is clicked ant then loads the corresponding card
+    var loadCard;
+    $(".button").click(function(){
+        var cardClicked=$(this).find("p").text();
+        if(cardClicked=="Profile")
+        {
+            loadCard="profile-card";
+            //alert(loadCard);
+        }
+        else if (cardClicked=="Experience")
+        {
+            loadCard="experience-card";
+            //alert(loadCard);
+        }
+        else if (cardClicked=="Contact me")
+        {
+            loadCard="contact-card";
+            //alert(loadCard);
+
+        }
+         else if (cardClicked=="Education")
+        {
+            loadCard="education-card";
+            //alert(loadCard);
+        }
+        else if (cardClicked=="Resume")
+        {
+            loadCard="resume-card";
+            //alert(loadCard);
+        }
+        // This runs a series of steps to see the transition animation yo see.
+        $("#"+loadCard).removeClass("display-none");
+        $("#mainMenu").attr("class","deactivate");
+        setTimeout(dothis,500);
+        $("#"+loadCard).addClass("popin");
+
+    });
+    function dothis(){
+        $("#mainMenu").addClass("display-none");
+    };
+    // Back button this disables the -card and loads the the mainMenu cards
+    $("#backButton").click(function(){
+        $("#"+loadCard).addClass("display-none");
+        $("#mainMenu").attr("class","active");
+
+    });
+
+});
