b109e377ac5995a7d8f42f33a8f9db3d23982f94
Dolapo Toki <dolapotoki@gmail.com>
2016-09-07 09:44:19 -0700

    Made design improvements

--- app/js/script.js
+++ app/js/script.js
@@ -7,13 +7,13 @@ $(document).ready(function () {
     var projectDataRecieved=false;
     $(".button").click(function(){
         var cardClicked=$(this).find("p").text();
-        if(cardClicked=="Profile")
+        if(cardClicked=="PROFILE")
         {
             loadCard="profile-card";
             colorChange="#009fd4"
             //alert(loadCard);
         }
-        else if (cardClicked=="Experience")
+        else if (cardClicked=="EXPERIENCE")
         {
             loadCard="experience-card";
             colorChange="#8c3"
@@ -23,21 +23,21 @@ $(document).ready(function () {
             
             //alert(loadCard);
         }
-        else if (cardClicked=="Contact me")
+        else if (cardClicked=="CONTACT ME")
         {
             loadCard="contact-card";
             colorChange="#e6c32d"
             //alert(loadCard);
 
         }
-         else if (cardClicked=="Education")
+         else if (cardClicked=="EDUCATION")
         {
             loadCard="education-card";
             colorChange="#e67e30"
             //colorChange="rgba(235, 116, 27, 0.87)"
             //alert(loadCard);
         }
-        else if (cardClicked=="Projects")
+        else if (cardClicked=="PROJECTS")
         {
             loadCard="project-card";
             colorChange="#de2643"
@@ -51,7 +51,7 @@ $(document).ready(function () {
             //console.log(JSON.stringify(window.getProjects()));
             // JSON.parse();
         }
-        else if (cardClicked=="Resume"){
+        else if (cardClicked=="RESUME"){
             loadCard="Null"
             var win = window.open("https://firebasestorage.googleapis.com/v0/b/dolapo-websiteapi.appspot.com/o/Dolapos%20Resume(2016)_updated_reduced.pdf?alt=media&token=9e84b5a3-4062-47c8-bd04-74a4f08f4dc1", '_blank');
             win.focus();
@@ -159,18 +159,22 @@ $(document).ready(function () {
            //console.log(expandedProjectTags);
         }
         
-        var doc='<div class="col-4of10 leftDisplayBox"> <iframe width="100" height="100" src="'+ videoUrl+'"></iframe> </div> <div class="col-6of10 rightDisplayBox cardSpacer"><div id="profile-cardMain"> <h1 class="leftTag" id="projectTitle">'+projectTitle+'</h1> <div class="underline"></div> <p class="paragraphWhite">'+projectDescription+'</p> <h1 class="leftTag" id="projectDetailsTitle"> Project details</h1> <div class="underline"></div> <div class="col-10of10" id="projectDetails"> <div class="col-3of10 projectDetailsItems" id="projectTagSection"> '+expandedProjectTags+'</div> <div class="col-3of10 projectDetailsItems" id="websiteSection"> <a href="'+projectUrl+'">Project website</a> </div> <div class="col-3of10 projectDetailsItems" id="gitProjectSection"> <a href="'+projectRepo+'" target="_blank" class="fade2"><img src="img/icons/github-logo.svg" width="30px"></a> </div> </div> </div> </div> </div>';
+        var doc='<div class="col-4of10 leftDisplayBox"> <iframe width="100" height="100" src="'+ videoUrl+'"></iframe> </div> <div class="col-6of10 rightDisplayBox cardSpacer"><div id="profile-cardMain"> <h1 class="leftTag projectTitle">'+projectTitle+'</h1> <div class="underline"></div> <p class="paragraphWhite kick">'+projectDescription+'</p> <h1 class="leftTag projectTitle"> Project details</h1> <div class="underline"></div> <div class="col-10of10" id="projectDetails"> <div class="col-3of10 projectDetailsItems marginProjectDetails" id="projectTagSection"> '+expandedProjectTags+'</div> <div class="col-3of10 projectDetailsItems" id="websiteSection"> <a href="'+projectUrl+'">Project website</a> </div> <div class="col-3of10 projectDetailsItems" id="gitProjectSection"> <a href="'+projectRepo+'" target="_blank" class="fade2"><img src="img/icons/github-logo.svg" width="30px"></a> </div> </div> </div> </div> </div>';
         var d1 = document.getElementById('project-card');
         d1.insertAdjacentHTML('beforeend', doc);
         projectDataRecieved=true;
        
     }
     function createExperienceCard(image,company,role,workDetails){
-        var doc='<div class="col-4of10 leftDisplayBox"> <img  class="companyImage" src="img/skynation.png" alt="Company image"> </div> <div class="col-6of10 rightDisplayBox cardSpacer"> <!--<span class="glyphicon glyphicon-user white" aria-hidden="true"></span>--> <div id="profile-cardMain"> <!--<p class="leftTag">Profile</p>--> <!--<div class="underline"></div>--> <table> <tr> <td class="leftColumn">Comapany</td> <td class="rightColumn">'+company+'</td> </tr> <tr class="spacer"></tr> <tr> <td class="leftColumn">Role</td> <td class="rightColumn">'+role+'</td> </tr> <tr class="spacer"></tr> <!--<tr> <td class="leftColumn">Ocupation</td> <td class="rightColumn"> Student </td> </tr>--> <tr class="spacer"></tr> </table> <p class="leftTag">Work Details</p> <div class="underline"></div> <p id="kick">'+workDetails+'</p> </div> </div> ';
+        var doc='<div class="col-4of10 leftDisplayBox"> <img  class="companyImage" src="img/skynation.png" alt="Company image"> </div> <div class="col-6of10 rightDisplayBox cardSpacer"> <!--<span class="glyphicon glyphicon-user white" aria-hidden="true"></span>--> <div id="profile-cardMain"> <!--<p class="leftTag">Profile</p>--> <!--<div class="underline"></div>--> <table> <tr> <td class="leftColumn">Comapany</td> <td class="rightColumn">'+company+'</td> </tr> <tr class="spacer"></tr> <tr> <td class="leftColumn">Role</td> <td class="rightColumn">'+role+'</td> </tr> <tr class="spacer"></tr> <!--<tr> <td class="leftColumn">Ocupation</td> <td class="rightColumn"> Student </td> </tr>--> <tr class="spacer"></tr> </table> <p class="leftTag">Work Details</p> <div class="underline"></div> <p class="kick">'+workDetails+'</p> </div> </div> ';
         var d2 = document.getElementById('experience-card');
         d2.insertAdjacentHTML('beforeend',doc);
         experiencDataRecieved=true;
     }
+
+    
     
 
 });
+
+   
